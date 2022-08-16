25f711a969c282c5bc9b2aa8c1da995830da62a3
Emily Tregelles <etregelles@gmail.com>
2022-08-16 19:11:24 +0100

    move Slider handleChange to Metronome.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,21 +1,11 @@
 import './Slider.css';
 import React from "react";
-import { useState } from 'react';
 
 export class Slider extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: '30'};
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
   render() {
     return (
       <div className="slider-parent">
-        <p className="bpmOutput">{this.state.value} BPM</p>
-        <input type="range" min="30" max="244" value={this.state.value} onChange={this.handleChange}  />
+        <input type="range" min="60" max="240" value={this.props.bpm} onChange={this.props.onChange}/>
       </div>
     );
   }
